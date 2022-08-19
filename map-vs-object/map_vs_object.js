f0aca83f81d63263bf68f27418af280344e58173
Zaid Rashid <zed182@gmail.com>
2022-08-19 13:09:50 +0200

    testing - not the ideal test. timer changes based on the order of test

--- map-vs-object/map_vs_object.js
+++ map-vs-object/map_vs_object.js
@@ -1,29 +1,41 @@
 // https://bretcameron.medium.com/how-javascript-maps-can-make-your-code-faster-90f56bf61d9d
-
+// problem. if you switch around the cases, it does not work. Object is faster
 let result;
 
 let obj = {};
 let map = new Map(), n = 1000000;
-console.time('Object-create');
+// console.time('ObjectCreate');
 for (let i = 0; i < n; i++) {
   obj[i] = i;
 }
-console.timeEnd('Object-create');
+// console.timeEnd('ObjectCreate');
 
-console.time('Map-create');
+// console.time('MapCreate');
 for (let i = 0; i < n; i++) {
   map.set(i, i);
 }
-console.timeEnd('Map-create');
+// console.timeEnd('MapCreate');
+
+let start, end;
+
+console.time('map');
+start = performance.now();
+result = map.has(999999);
+end = performance.now();
+console.log(end-start);
+console.timeEnd('map');
 
 console.time('Object');
+start = performance.now();
 result = obj.hasOwnProperty('999999');
+end = performance.now();
+console.log(end-start);
 console.timeEnd('Object');
-// console.log(Object.keys(obj).length);
 
-console.time('Map');
-result = map.has(999999);
-console.timeEnd('Map');
-// console.log(map.size);
+
+
+
+
+
 
 
